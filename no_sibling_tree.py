ab6e06040bf62b90c9b96a6cd631cc054ff614b0
Amogh Singhal <devamoghs@gmail.com>
2018-02-18 15:43:32 +0530

    Create no_sibling_tree.py

---
+++ no_sibling_tree.py
@@ -0,0 +1,32 @@
+# Problem: Print the nodes of a binary tree
+# which do not have a sibling
+
+class Node:
+  
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+  
+def printSingleNode(root, hasSibling):
+  # hasSibling will check if root has both children
+  if root is None:
+    return
+  else:
+    # if root has one child, print that child data
+    if(not hasSibling):
+      print("%d"%root.data)
+      
+    printSingleNode(root.left, root.right is not None)
+    printSingleNode(root.right, root.left is not None)
+
+    
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.right.left = Node(5)
+root.right.left.left = Node(6)
+ 
+print("Level order traversal of binary tree is -")
+printSingleNode(root, True)
