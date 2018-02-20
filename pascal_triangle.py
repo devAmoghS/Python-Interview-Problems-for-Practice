06e895f6045145d0921f3437d9dde9a2bc750de0
Amogh Singhal <devamoghs@gmail.com>
2018-02-20 17:55:42 +0530

    Create pascal_triangle.py

---
+++ pascal_triangle.py
@@ -0,0 +1,37 @@
+# Recusrive method to create the series
+def computePascal(col, row):
+  if col == row or col == 0:
+    return 1
+  else:
+    return computePascal(col-1, row-1) + computePascal(col, row-1)
+    
+# Method to create the triangle for `N` row
+def printTriangle(num):
+  for r in range(num):
+    for c in range(r+1):
+      print(str(computePascal(c, r)), end=' ')
+    print('\n')
+    
+printTriangle(10)
+'''
+Output:
+1 
+
+1 1 
+
+1 2 1 
+
+1 3 3 1 
+
+1 4 6 4 1 
+
+1 5 10 10 5 1 
+
+1 6 15 20 15 6 1 
+
+1 7 21 35 35 21 7 1 
+
+1 8 28 56 70 56 28 8 1 
+
+1 9 36 84 126 126 84 36 9 1 
+'''
