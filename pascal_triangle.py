8113b25d2a452fde49b126aedb5b853c4d051fb6
Amogh Singhal <devamoghs@gmail.com>
2018-02-20 18:16:43 +0530

    Update pascal_triangle.py

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,13 +1,19 @@
 # Recusrive method to create the series
 def computePascal(col, row):
+  # There are three things to compute
+  # 1. Left edge: col is 0
+  # 2. Right edge: col is same as row
   if col == row or col == 0:
     return 1
+  # 3. any other cell: col-1 + col of the previous row
   else:
     return computePascal(col-1, row-1) + computePascal(col, row-1)
     
 # Method to create the triangle for `N` row
 def printTriangle(num):
   for r in range(num):
+    # upon observation, we can deduce the relation
+    # num_cols = num_rows + 1
     for c in range(r+1):
       print(str(computePascal(c, r)), end=' ')
     print('\n')
