e9aa07c9f888869e5952a5207a11d74857b0e04e
Amogh Singhal <devamoghs@gmail.com>
2018-02-25 01:03:24 +0530

    Create lowest_common_ancestor.py

---
+++ lowest_common_ancestor.py
@@ -0,0 +1,9 @@
+def findLowestCommonAncestor(root, value1, value2):
+  while root is not None:
+    value = root.value
+    if value > value1 and value > value2:
+      root = root.left
+    elif value < value1 and value < value2:
+      root = root.right
+    else:
+      return root
