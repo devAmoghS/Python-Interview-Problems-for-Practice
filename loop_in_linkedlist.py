0888074c6091461953fbe1c7b7b336e7d90382fd
Amogh Singhal <devamoghs@gmail.com>
2018-02-24 22:15:44 +0530

    Create loop_in_linkedlist.py

---
+++ loop_in_linkedlist.py
@@ -0,0 +1,19 @@
+def hasLoop(l_list):
+  fast = l_list.head.next_node
+  slow = l_list.head
+  hasLoop = False
+
+  while True:
+    if fast or fast.next_node:
+      pass
+    elif fast == slow or fast.next_node == slow:
+      hasLoop = True
+    else:
+      fast = fast.next_node
+      slow = slow.next_node
+    
+  return hasLoop
+  
+linked_list = LinkedList()
+# Returns the third element from last
+print(findMToLast(linked_list))
