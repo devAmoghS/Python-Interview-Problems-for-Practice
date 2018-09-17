bd36c9a91c3d54768a2c1a3da3d9f58d22198330
Amogh Singhal <devamoghs@gmail.com>
2018-09-17 22:45:39 +0530

    Create first_recurring_character.py

---
+++ first_recurring_character.py
@@ -0,0 +1,16 @@
+def first_recurring_character(input):
+    flag = None
+    d = dict()
+    for char in input:
+        if char in d.keys():
+            flag = char
+            return flag
+        d[char] = 1
+    return flag
+    
+result = first_recurring_character("DFGHJWERGBFGHJ")
+print(result)   # G
+result = first_recurring_character("ABCDEFGH")
+print(result)   # None
+result = first_recurring_character("12345642124345")
+print(result)   # 4
