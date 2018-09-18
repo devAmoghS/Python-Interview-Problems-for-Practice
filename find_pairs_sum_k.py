8586bcfe5030f3032fa81aa6e6c7562ce74c3a4e
Amogh Singhal <devamoghs@gmail.com>
2018-09-18 09:43:01 +0530

    Create find_pairs_sum_k.py

---
+++ find_pairs_sum_k.py
@@ -0,0 +1,9 @@
+def find_pairs(num_array, k):
+  pairs_array = []
+  for num in num_array:
+    if (k-num) in num_array:
+      pairs_array.append((num, (k-num)))
+  return pairs_array
+
+result = find_pairs([0, 14, 0, 4, 7, 8, 3, 5, 7], 11)
+print(result)
