1ff5a1b96cc75b976af94e39de31f5a8925b8eb6
Amogh Singhal <devamoghs@gmail.com>
2019-04-11 01:58:43 +0530

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,47 +1,39 @@
-def swap(a, b):
-  temp = a
-  a = b
-  b = temp
+def quick_sort(arr):
+    quick_sort_helper(arr, 0, len(arr)-1)
 
-def quick_sort(array):
-  quick_sort_helper(array, 0, len(array)-1)
 
 def quick_sort_helper(arr, first, last):
-  if first < last:
-    split_point = partition(arr, first, last)
+    if first < last:
+        pi = partition(arr, first, last)
 
-    quick_sort_helper(arr, first, split_point - 1)
-    quick_sort_helper(arr, split_point + 1, last)
+        quick_sort_helper(arr, first, pi-1)
+        quick_sort_helper(arr, pi+1, last)
+
 
 def partition(arr, first, last):
-  pivot = arr[first]
+    pivot = arr[first]
 
-  left = first + 1
-  right = last
+    left = first+1
+    right = last
 
-  done = False
+    done = False
+    while not done:
+        while left <= right and arr[left] <= pivot:
+            left += 1
 
-  while not done:
-    
-    # move left and right pointers to locate split_point
-    while left <= right and arr[left] <= pivot:
-      left += 1
-    while arr[right] >= pivot and right >= left:
-      right -= 1
+        while arr[right] >= pivot and right >= left:
+            right -= 1
 
-    # when both pointers cross each other
-    # we have located the split_point
-    if right < left:
-      done = True
-    else:
-      # swap the left and right elements
-      swap(arr[left], arr[right])
+        if right < left:
+            done = True
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
 
-  # swap the first and right elements
-  swap(arr[first], arr[right])
+    arr[first], arr[right] = arr[right], arr[first]
 
-  return right
+    return right
 
-  array = [54,26,93,17,77,31,44,55,20]
-  quick_sort(array)
-  print(array)
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+quick_sort(alist)
+print(alist)  # [17, 20, 26, 31, 44, 54, 55, 77, 93]
