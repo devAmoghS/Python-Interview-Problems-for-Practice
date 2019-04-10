00499ccccf4a9a059d2509746ec59580d0472675
Amogh Singhal <devamoghs@gmail.com>
2019-04-11 02:13:53 +0530

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -2,4 +2,4 @@ def remove_duplicates(arr):
   return list(dict.fromkeys(arr))
 
 result = remove_duplicates([0,0,0,1,1,2,2,3,4,5])
-print(result)   # [1, 2, 3, 4, 5]
+print(result)   # [0, 1, 2, 3, 4, 5]
