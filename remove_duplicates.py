53b746993fe124aa02a20daca3569130e61b29c7
Amogh Singhal <devamoghs@gmail.com>
2019-04-11 02:13:34 +0530

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,11 +1,5 @@
-def remove_duplicates(num_array):
-  d = dict()
-  for idx in range(0, len(num_array)):
-    if num_array[idx] in d.keys():
-      num_array[idx] = 0
-    d[num_array[idx]] = 1
-  dedupe_array = [n for n in num_array if n != 0]
-  return dedupe_array
+def remove_duplicates(arr):
+  return list(dict.fromkeys(arr))
 
-result = remove_duplicates([1, 1, 2, 2, 3, 4, 5])
+result = remove_duplicates([0,0,0,1,1,2,2,3,4,5])
 print(result)   # [1, 2, 3, 4, 5]
