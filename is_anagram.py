97eef9edb4752ef2d73718f8c0fb494739b9282a
Amogh Singhal <devamoghs@gmail.com>
2019-06-04 15:47:16 +0530

    Update is_anagram.py

--- is_anagram.py
+++ is_anagram.py
@@ -1,16 +1,18 @@
 # Check if two strings are anagrams of each other
 
-def is_anagram(str1, str2):
+def anagramise(word):
+  d = dict()
 
-  if len(str1) == len(str2):
-    for i in str1:
-      if i in str2:
-        pass
-      else:
-        return False
-    return True
-  else:
-    return False
+  for char in word:
+    if char not in d.keys():
+      d[char] = 1
+    else:
+      d[char] += 1
+  
+  return d
+
+def is_anagram(str1, str2):
+  return anagramise(str1) == anagramise(str2)
 
 result = is_anagram("hello", "billion")
 print(result)  # False
