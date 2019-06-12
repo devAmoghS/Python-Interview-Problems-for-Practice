13bbed8ff9a4422af575f4e0a54cc6e5cefea907
Rhys Thomas <rhys97@me.com>
2019-06-12 12:02:22 +0100

    Added `self` to function def

--- heap_structure.py
+++ heap_structure.py
@@ -1,13 +1,13 @@
 class Heap(object):
-  
+
   HEAP_SIZE = 10;
-  
+
   def __init__(self):
     self.heap = [0]*Heap.HEAP_SIZE
     self.currentPosition = -1
-    
+
   def insert(self, item):
-    
+
     # if heap is full , we print a notification
     if self.isFull():
       print("Heap is full")
@@ -16,7 +16,7 @@ class Heap(object):
     self.currentPosition+=1
     self.heap[self.currentPosition] = item
     self.fixUp(self.currentPosition)
-    
+
   def fixUp(self, index):
     parentIndex = int((index-1)/2)
     while parentIndex >=0 and self.heap[parentIndex] < self.heap[index]:
@@ -27,15 +27,15 @@ class Heap(object):
       # update the index and parentIndex
       index = parentIndex
       parentIndex = int((index-1)/2)
-  
+
   def fixDown(self, index, upto):
     if upto < 0:
       upto = self.currentPosition
-      
+
     while index<=upto:
       leftChild = 2*index+1
       rightChild = 2*index+2
-      
+
       if leftChild <= upto:
         childToSwap = 0
       else:
@@ -43,19 +43,19 @@ class Heap(object):
           childToSwap = leftChild
         else:
           childToSwap = rightChild
-      
+
       if self.heap[index] < self.heap[childToSwap]:
         temp = self.heap[index]
         self.heap[index] = self.heap[childToSwap]
         self.heap[childToSwap] = temp
       else:
         break
-      
+
       index = childToSwap
-      
+
     else:
       return
-    
+
   def heapSort(self):
     for i in range(0, self.currentPosition+1):
       temp = self.heap[0]
@@ -63,18 +63,18 @@ class Heap(object):
       self.heap[0] = self.heap[self.currentPosition-i]
       self.heap[self.currentPosition-i] = temp
       self.fixDown(0, self.currentPosition-i-1)
-      
-  def getMax():
+
+  def getMax(self):
     result = self.heap[0]
     self.currentPosition-=1
     self.heap[0] = self.heap[self.currentPosition]
     del self.heap[self.currentPosition]
     self.fixDown(0, -1)
     return result
-    
+
   def isFull(self):
     return self.currentPosition == Heap.HEAP_SIZE
-    
+
 some_heap=Heap()
 some_heap.insert(12)
 some_heap.insert(-3)
