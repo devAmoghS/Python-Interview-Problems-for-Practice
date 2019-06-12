10815b32dd2dde4834fe3e1bba942cf95439efe6
Rhys Thomas <rhys97@me.com>
2019-06-12 12:03:21 +0100

    Imported LinkedList data structure

--- find_m_to_last_llist.py
+++ find_m_to_last_llist.py
@@ -3,21 +3,23 @@
 # 2->3->4->8->5; m=2 will return 8
 # since 8 is second to last
 
+from linked_list_data_structure import LinkedList
+
 def findMToLast(l_list, m):
   current = l_list.head
   count = 0
-  
+
   while current is not None and count < m:
     count+=1
     current = current.getNextNode()
-  
+
   m_behind = l_list.head
   while current.next_node is not None:
     current = current.getNextNode()
     m_behind = m_behind.getNextNode()
-    
+
   return m_behind
-  
+
 linked_list = LinkedList()
 m_to_last = 3
 # Returns the third element from last
