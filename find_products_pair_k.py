de1072cee08744f5d8ebc175bb767a4226982236
Amogh Singhal <devamoghs@gmail.com>
2019-07-27 21:24:18 +0530

    Create find_products_pair_k.py

---
+++ find_products_pair_k.py
@@ -0,0 +1,16 @@
+def product_pair(arr, x):
+  arr_sorted = sorted(arr)
+
+  for i in range(0, len(arr_sorted)):
+    sub_array = arr_sorted[i+1:]
+    if x // arr_sorted[i] in sub_array:
+      return True
+
+  return False
+
+
+arr = [10, 20, 9, 40]  
+x = 400
+
+res = product_pair(arr, x)
+print(res)
