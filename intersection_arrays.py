27361693a489bee9c3ad6379eb1bc7383fe71ddf
Amogh Singhal <devamoghs@gmail.com>
2019-09-15 10:42:37 +0530

    Update intersection_arrays.py

--- intersection_arrays.py
+++ intersection_arrays.py
@@ -3,24 +3,24 @@
 # common elements between them
 # Constraints: in O(m+n) complexity.
 
-
 def inr(z):
     return z + 1
 
-
 def intersectionArrays(x, y, m, n):
     i, j = 0, 0
+    intersect_arr = []
+
     while i < m and j < n:
         # print(i, j)
         if x[i] == y[j]:
-            print(x[i])
-            i = inr(i)
-            j = inr(j)
+            intersect_arr.append(x[i])
+            i, j = inr(i), inr(j)
         elif x[i] < y[j]:
             i = inr(i)
         else:
-            j = inr[j]
-
+            j = inr(j)
+    print(intersect_arr)
+    return
 
 list_a = [1, 2, 3, 4, 5]
 list_b = [2, 3, 5, 6]
