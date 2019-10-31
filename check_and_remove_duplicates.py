03511ff1f97785a944c43d68f88de1f0ddfef0d7
vaishnavprachi98 <pvaishnav.98@gmail.com>
2019-10-31 18:42:59 +0530

    Added a program to check number of duplicates and remove them

---
+++ check_and_remove_duplicates.py
@@ -0,0 +1,11 @@
+import collections
+a = [1,1,1,1,2,2,2,2,3,3,4,5,5]
+counter=collections.Counter(a)
+print(counter)
+# Counter({1: 4, 2: 4, 3: 2, 5: 2, 4: 1})
+print(counter.values())
+# [4, 4, 2, 1, 2]
+print(counter.keys())
+# [1, 2, 3, 4, 5]
+print(counter.most_common(3))
+# [(1, 4), (2, 4), (3, 2)]
