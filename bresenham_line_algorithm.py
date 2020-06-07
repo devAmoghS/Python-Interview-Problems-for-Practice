f0d293d2c3ca63002db754ef268dba1fe89a34d8
Amogh Singhal <devamoghs@gmail.com>
2020-06-07 10:38:10 +0530

    Update bresenham_line_algorithm.py

--- bresenham_line_algorithm.py
+++ bresenham_line_algorithm.py
@@ -1,3 +1,18 @@
+# Bresenham Line Algorithm (BLA) is one of the earliest algorithms developed
+# in computer graphics. It is used for drawing lines.  It is an efficient method because 
+# it involves only integer addition, subtractions, and multiplication operations. 
+
+# These operations can be performed very rapidly so lines can be generated quickly.
+
+# Reference: http://floppsie.comp.glam.ac.uk/Southwales/gaius/gametools/6.html
+
+# Algorithm:
+# 1. We are given the starting and ending point (x1, y1) and (x2, y2)
+# 2. We compute the gradient m, using the formula: m = (y2-y1)/(x2-x1)
+# 3. The equation of the straight line is y = m*x+c. So the next thing we need to find is the intercept c
+# 4. Intercept can be derived using the formula c = y1 - m*x1
+# 5. To get the next point, we add dx to the x-cordinate and dy to the y cordinate
+
 def lineGenerator(x1, y1, x2, y2):
 	dx = x2 - x1
 	dy = y2 - y1
@@ -26,25 +41,25 @@ def lineGenerator(x1, y1, x2, y2):
 			slope = slope
 
 
-lineGenerator(3, 2, 15, 5)
+# lineGenerator(3, 2, 15, 5)
 
-	# if P1[0]==P2[0] and P1[1]==P2[1]:
-	# 	return 0
-	# #P1 is the point given. Initial point
-	# #P2 is the point to reach. Final point
-	# print(P1)
-	# #Check if the point is above or below the line.
-	# dx = P2[0]-P1[0]
-	# dy = P2[1]-P1[0]
+# 	# P1 is the point given. Initial point
+# 	# P2 is the point to reach. Final point
+# 	if P1[0] == P2[0] and P1[1] == P2[1]:
+# 		return 0
+# 	print(P1)
+# 	#Check if the point is above or below the line.
+# 	dx = P2[0]-P1[0]
+# 	dy = P2[1]-P1[0]
 
-	# di = 2*dy - dx
+# 	di = 2*dy - dx
 
-	# currX = P1[0]
-	# currY = P1[1]
+# 	currX = P1[0]
+# 	currY = P1[1]
 
-	# if di > 0:
-	# 	P1 = (currX+1, currY+1)
-	# else:
-	# 	P1 = (currX+1, currY)
+# 	if di > 0:
+# 		P1 = (currX+1, currY+1)
+# 	else:
+# 		P1 = (currX+1, currY)
 
-	# return lineGenerator(P1, P2)
+# 	return lineGenerator(P1, P2)
