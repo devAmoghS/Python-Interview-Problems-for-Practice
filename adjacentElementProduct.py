4fe0e73e4d8ec5314cf852d28644d1bf8500344b
Amogh Singhal <devamoghs@gmail.com>
2020-06-07 10:18:50 +0530

    Update adjacentElementProduct.py

--- adjacentElementProduct.py
+++ adjacentElementProduct.py
@@ -1,7 +1,40 @@
-#Given an array of integers,
-#find the pair of adjacent elements 
-#that has the largest product and 
-#return that product.
+# Given an array of integers, find the pair of adjacent elements 
+# that has the largest product and return that product.
+
+# Approach 1: (Brute Force) - Check all the pairs in the list and then return the maximum pair
+# Time Complexity: O(N^2)
+
+def adjacentElementProductBF(inputArray):
+	largestProduct = -999999
+	
+	# for sanity check, assert if array contains at least 2 elements
+	if len(inputArray) < 2:
+		print("No pairs exists")
+		return -1
+	
+	for i in range(0, len(inputArray)):
+		for j in range(i+1, len(inputArray)):
+			currentProduct = inputArray[i]*inputArray[j]
+			
+			if currentProduct > largestProduct:
+				largestProduct = currentProduct
+	
+	return largestProduct
+
+# Approach 2: (Sort & Pick Last Pair) - Sort the list and then pick the last two numbers
+# Caveat: All elements must be positive
+# Time Complexity: O(Nlog(N))
+
+def adjacentElementsProductSort(inputArray):
+	size = len(inputArray)
+	
+	if size < 2:
+		print("No Pairs exist")
+		return -1
+	
+	sortedArray = sorted(inputArray)
+	return sortedArray[-1] * sortedArray[-2] 
+
 
 def adjacentElementsProduct(inputArray):	
 
@@ -24,11 +57,3 @@ print(adjacentElementsProduct([3, 6, -2, -5, 7, 3]))
     
 #Alternate solution
 #return max([inputArray[i]*inputArray[i+1] for i in range(0, int(len(inputArray)-1))])
-
-
-
-
-
-
-
-
