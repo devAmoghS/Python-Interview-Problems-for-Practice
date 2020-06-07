433721f1c5bc1a891cd9f3e4516143091235c0bc
Amogh Singhal <devamoghs@gmail.com>
2020-06-07 11:57:34 +0530

    Update bst_nodes_in_range.py

--- bst_nodes_in_range.py
+++ bst_nodes_in_range.py
@@ -1,7 +1,12 @@
-# Problem: Find the no. of nodes in a BST
-# that lies in a given range
+# Problem: Find the no. of nodes in a BST that lies in a given range
 
+# Algorithm: We will traverse the tree recursively until we encounter leaf nodes (Base case)
+# else we do the following
+# 1. Current node is less than the given range --> Traverse the right subtree
+# 2. Current node is more than the given range --> Traverse the left subtree
+# 3. Current node lies in the given range      --> Increment Count; Traverse both the left and right subtree
 
+# Data Structure for Tree Node
 class Node:
     def __init__(self, data):
         self.data = data
@@ -29,13 +34,14 @@ def nodesWithinRange(root, range):
     elif root.data < low:
         return nodesWithinRange(root.right, range)
 
+if __name__ == "main":
+    
+    node = Node(10)
+    node.left = Node(5)
+    node.left.left = Node(1)
+    node.right = Node(50)
+    node.right.left = Node(45)
+    node.right.right = Node(100)
 
-node = Node(10)
-node.left = Node(5)
-node.left.left = Node(1)
-node.right = Node(50)
-node.right.left = Node(45)
-node.right.right = Node(100)
-
-result = nodesWithinRange(node, (5, 45))
-print(result)
+    result = nodesWithinRange(node, (5, 45))
+    print(result)
