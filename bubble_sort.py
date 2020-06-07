a731ee4400109e904a9783aac0b775dae0b84fdb
Amogh Singhal <devamoghs@gmail.com>
2020-06-07 16:10:07 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,40 @@
 #  A simple implementation of bubble sort
 
 def bubbleSort(arr):
+  # travese the whole array
   for i in range(len(arr)):
-    for j in range(i+1, len(arr)):
-      if arr[i] > arr[j]:
-        arr[i], arr[j] = arr[j], arr[i]
+    
+    # last i elements are already in place
+    for j in range(0, len(arr)-i-1):
+      
+      if arr[j] > arr[j+1]:
+        arr[j], arr[j+1] = arr[j+1], arr[j]
   
   return arr
 
-arr = [2, 6, 1, 5, 3, 4]
-res = bubbleSort(arr)
-print(res)
+# Approach 2: This algorithm will run for O(n^2) even if the array is
+# already sorted. For avoiding this, we can check if elements are swapped
+# in each pass. We will break the loop in case they are not
+
+# Time Complexity: O(n^2) - Average or Worst Case; O(n) - Best case [Array is already sorted]
+
+def bubbleSortOptimized(arr):
+  for i in range(len(arr)):
+    swapped = False
+    
+    for j in range(0, len(arr)-i-1):
+      
+      if arr[i] > arr[j]:
+        arr[i], arr[j] = arr[j], arr[i]
+        swapped = True
+    
+    # if no elements are swapped, break the loop
+    if swapped == False:
+      break
+  
+  return arr
+
+if __name__ = "__main__":
+  arr = [2, 6, 1, 5, 3, 4]
+  res = bubbleSort(arr)
+  print(res)
