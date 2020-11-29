2e413870cdacd3ba96fddce0a3af1fda7820d160
Amogh Singhal <devamoghs@gmail.com>
2020-11-29 20:03:17 +0530

    Update interger_to_roman_num.py

--- interger_to_roman_num.py
+++ interger_to_roman_num.py
@@ -34,19 +34,19 @@ print(roman)
 # Explaination:
 # ===============
 # 289
-# 289//1000 --> 0, i=1
-# 289//900 --> 0, i=2
-# 289//500 --> 0, i=3
-# 289//400 --> 0, i=4
-# 289//100 --> 2, then i=4, result_str = 'C', input_num = 189
-# 198//100 --> 1, then i=4, result_str='CC', input_num = 89
-# 98//100 --> 0, then i=5
-# 98//50 --> 1, then i=6, result_str='CCL', input_num= 39
-# 39//50 --> 0, then i=6
-# 39//40 --> 0, then i=7
-# 39//10 --> 3, then i=8, result_str='CCLX', input_num=29
-# 29//10 --> 3, then i=8, result_str='CCLXX', input_num=19
-# 19//10 --> 3, then i=8, result_str='CCLXXX', input_num=9
-# 9//10 --> 0, then i=9
-# 9//9 --> 1, then i=9, result_str='CCLXXXIX', input_num=0
+# 289//1000   --> 0, i=1
+# 289//900    --> 0, i=2
+# 289//500    --> 0, i=3
+# 289//400    --> 0, i=4
+# 289//100    --> 2, then i=4, result_str = 'C',        input_num = 189
+# 189//100    --> 1, then i=4, result_str='CC',         input_num = 89
+# 89//100     --> 0, then i=5
+# 89//50      --> 1, then i=6, result_str='CCL',        input_num= 39
+# 39//50      --> 0, then i=6
+# 39//40      --> 0, then i=7
+# 39//10      --> 3, then i=8, result_str='CCLX',       input_num=29
+# 29//10      --> 3, then i=8, result_str='CCLXX',      input_num=19
+# 19//10      --> 3, then i=8, result_str='CCLXXX',     input_num=9
+# 9//10       --> 0, then i=9
+# 9//9        --> 1, then i=9, result_str='CCLXXXIX',   input_num=0
 # EXIT
