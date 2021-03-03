91f314bbbb3a408a9e68f79c259659cd49cbc54f
Amogh Singhal <devamoghs@gmail.com>
2021-03-03 10:02:03 +0530

    Create hundred_without_int.py

---
+++ hundred_without_int.py
@@ -0,0 +1,21 @@
+# Print numbers 1 to 100 without using any numbers or integers
+
+ONE = str(int(True))
+ZERO = str(int(False))
+HUNDRED  = int(ONE + ZERO + ZERO)
+
+for i in range(int(ONE), HUNDRED+1):
+  print(i, end=', ')
+
+# OUTPUT (Actual prints in the same line, line breaks given here for code clarity):
+
+# 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+# 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+# 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+# 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
+# 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 
+# 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 
+# 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 
+# 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 
+# 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 
+# 91, 92, 93, 94, 95, 96, 97, 98, 99, 100,
