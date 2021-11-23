a32bf7797b889c8cd141e2b1cebde33b312cb7b1
Amogh Singhal <singhal.amogh1995@gmail.com>
2021-11-23 14:51:16 +0530

    Update practice.py

--- bits_wilp/practice.py
+++ bits_wilp/practice.py
@@ -17,3 +17,20 @@ def get_freq(str):
 result = get_freq("Amogh loves to eat apple and mango. His sister also loves eating apple and mango")
 print(result)
 # {'Amogh': 1, 'loves': 2, 'to': 1, 'eat': 1, 'apple': 2, 'and': 2, 'mango.': 1, 'His': 1, 'sister': 1, 'also': 1, 'eating': 1, 'mango': 1}
+
+
+
+def is_prime(num):
+  flag = 1
+
+  for i in range(2, num//2):
+    if num % i == 0:
+      flag = 0
+      break
+
+  if flag == 0:
+    print(f"{num} is not prime...")
+  else:
+    print(f"{num} is prime...")
+
+is_prime(7919) # 7919 is prime
